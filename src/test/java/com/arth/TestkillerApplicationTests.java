b01de8ff9bee289d534773efd5e1f2f41dd19c75
testkiller1 <kandarpdhimar018@gmail.com>
2022-02-10 09:42:16 +0530

    init

---
+++ src/test/java/com/arth/TestkillerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestkillerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

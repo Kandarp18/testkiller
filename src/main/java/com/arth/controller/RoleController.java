c8908fc6ed26e09aefe4f16dcf7ec90f7045c412
testkiller1 <kandarpdhimar018@gmail.com>
2022-02-16 09:01:17 +0530

    init

--- src/main/java/com/arth/controller/RoleController.java
+++ src/main/java/com/arth/controller/RoleController.java
@@ -1,5 +1,6 @@
 package com.arth.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -7,19 +8,25 @@ import org.springframework.web.bind.annotation.PostMapping;
 //import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.arth.bean.RoleBean;
+import com.arth.dao.RoleDao;
 
 @Controller
 public class RoleController {
+	@Autowired
+	RoleDao roleDao;
+
 
 //	@RequestMapping(value ="newrole",method = RequestMethod.GET)
 	@GetMapping("/newrole")
 	public String newRole() {
-		return "NewRole";// jsp
+		return "NewRole";
 	}
 
 	@PostMapping("/saverole")
 	public String saveRole(RoleBean role) {
 		System.out.println(role.getRoleName());
+        roleDao.insertRole(role);
+		
 		return "";
 	}
 }
